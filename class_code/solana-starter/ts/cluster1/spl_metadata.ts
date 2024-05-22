4aeadc0d31da8700530e1d275473ab912e113cfd
kilogold <bonilla_kelvin@hotmail.com>
2024-05-22 17:42:28 -0400

    metadata work

--- class_code/solana-starter/ts/cluster1/spl_metadata.ts
+++ class_code/solana-starter/ts/cluster1/spl_metadata.ts
@@ -4,12 +4,13 @@ import {
     createMetadataAccountV3, 
     CreateMetadataAccountV3InstructionAccounts, 
     CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+    DataV2Args,
 } from "@metaplex-foundation/mpl-token-metadata";
 import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import bs58 from 'bs58'
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("4FHnUReHAwQ29wpjRxJCphWYyjKMsgiAzU94M1f1tCVx")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -20,28 +21,42 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint: mint,
+            mintAuthority: signer,
+        };
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "TokeN",
+            symbol: "TKN",
+            uri: "https://www.example.com",
+            sellerFeeBasisPoints: 0,
+            creators: null,
+            collection: null,
+            uses: null,
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data: data,
+            isMutable: true,
+            collectionDetails: null,
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
-        // console.log(result);
+        let result = await tx.sendAndConfirm(umi).then(r => r.signature);
+        console.log(result);
+        console.log(bs58.encode(Buffer.from(result)));
+
+        // Result:
+        // https://explorer.solana.com/tx/5yiFALg7vmYi64BJd1FU6FthuqCHZBSkUZYovkiUrrJHujdcHWeZQEk7MEH42XATMaGighZNUuHivuWwiHmfB2Ja?cluster=devnet
+        
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
