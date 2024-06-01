2ef6358a99730635ce51e57abe2789c87338c8dc
kilogold <bonilla_kelvin@hotmail.com>
2024-06-01 14:44:31 -0400

    Mint account init.

--- tests/sol-crashers-on-chain.ts
+++ tests/sol-crashers-on-chain.ts
@@ -1,8 +1,10 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolCrashersOnChain } from "../target/types/sol_crashers_on_chain";
-import { PublicKey, Keypair } from "@solana/web3.js";
+import { PublicKey, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { ASSOCIATED_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
+import { getOrCreateAssociatedTokenAccount, createTransferInstruction, Account } from "@solana/spl-token";
+
 const TOKEN_2022_PROGRAM_ID = new anchor.web3.PublicKey(
   "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb"
 );
@@ -15,19 +17,27 @@ describe("sol-crashers-on-chain", () => {
 
   const payerPK = anchor.getProvider().publicKey;
 
-  const [pda_mint] = PublicKey.findProgramAddressSync(
+  const [pda_mint_gold] = PublicKey.findProgramAddressSync(
     [
-      anchor.utils.bytes.utf8.encode("mint"),
-      anchor.utils.bytes.utf8.encode("gold"),
+      Buffer.from("mint"),
+      Buffer.from("gold"),
     ],
     program.programId
   );
 
+  const bob_account = Keypair.generate();
+  let bob_ata_gold: Account;
+
   it("airdrop payer", async () => {
     await anchor.getProvider().connection.confirmTransaction(
-      await anchor.getProvider().connection.requestAirdrop(payerPK, 999999900000000000),
+      await anchor.getProvider().connection.requestAirdrop(payerPK, 900 * LAMPORTS_PER_SOL),
       "confirmed"
     );
+
+    await anchor.getProvider().connection.confirmTransaction(
+      await anchor.getProvider().connection.requestAirdrop(bob_account.publicKey, 100 * LAMPORTS_PER_SOL),
+      "confirmed"
+    );  
   });
 
   it("Is initialized!", async () => {
@@ -35,12 +45,12 @@ describe("sol-crashers-on-chain", () => {
     const tx = await program.methods
       .initialize()
       .accountsStrict({
-          payer: payerPK,
-          mint: pda_mint,
-          systemProgram: anchor.web3.SystemProgram.programId,
-          associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
-          tokenProgram: TOKEN_2022_PROGRAM_ID,
-        })
+        payer: payerPK,
+        mint: pda_mint_gold,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
       .rpc({
         skipPreflight: true,
       });
@@ -48,20 +58,25 @@ describe("sol-crashers-on-chain", () => {
     console.log("Transaction signature: %s", tx);
     console.log("Program ID:\t%s", program.programId.toBase58());
     console.log("Payer:\t\t%s", payerPK.toBase58());
-    console.log("Mint Gems PK:\t%s", pda_mint.toBase58());
+    console.log("Gold Mint PK:\t%s", pda_mint_gold.toBase58());
   });
 
-  // it("Mints Gold" async () => {
-  //   const tx = await program.rpc.mintGold({
-  //     accounts: {
-  //       authority: payer,
-  //       mint: pda_mint,
-  //       to: payer,
-  //       systemProgram: anchor.web3.SystemProgram.programId,
-  //       tokenProgram: TOKEN_2022_PROGRAM_ID,
-  //     },
-  //   });
+  it("Create token accounts", async () => {
+    bob_ata_gold = await getOrCreateAssociatedTokenAccount(
+      anchor.getProvider().connection,
+      bob_account,
+      pda_mint_gold,
+      pda_mint_gold,
+      true,
+      undefined,
+      undefined,
+      TOKEN_2022_PROGRAM_ID,
+      ASSOCIATED_PROGRAM_ID
+    );
+    console.log("Gold Mint ATA Bob PK:\t%s", bob_ata_gold.address.toBase58());
+  });
 
-  //   console.log("Your transaction signature", tx);
-  // });
+  it("Mint assets", async () => {
+
+  });
 });
