d050c970d52041e5a83789695cbae0acde30b7c5
kilogold <bonilla_kelvin@hotmail.com>
2024-06-02 11:46:59 -0400

    Implementing asset trade

--- capstone/sol-crashers-on-chain/programs/sol-crashers-on-chain/src/instructions/manage_asset.rs
+++ capstone/sol-crashers-on-chain/programs/sol-crashers-on-chain/src/instructions/manage_asset.rs
@@ -3,13 +3,14 @@ use anchor_lang::
 ;
 
 use anchor_spl::{
-    token_2022::Token2022, 
-    token_2022::mint_to,
+    token_2022::{
+        Token2022, mint_to, burn
+    },
     token_interface::{
-        Mint, TokenAccount, MintTo
-    }}
-;
-use crate::{shop_catalog, state};
+        Mint, TokenAccount, MintTo, Burn
+    }
+};
+use crate::{state, CatalogItem};
 
 #[derive(Accounts)]
 pub struct ManageAsset<'info> {
@@ -48,7 +49,25 @@ pub struct ManageAsset<'info> {
 
 pub fn trade(ctx: Context<ManageAsset>, trade_index: u8) -> Result<()> {
     
-    let cpi_accounts = anchor_spl::token_interface::MintTo {
+    let trading_pair = ctx.accounts.shop_catalog.trading_pairs[trade_index as usize];
+
+    cpi_mint(
+        &ctx, 
+        trading_pair.to_item,
+        trading_pair.amount_to_item
+    )?;
+
+    cpi_burn(
+        &ctx, 
+        trading_pair.from_item,
+        trading_pair.amount_from_item
+    )?;
+
+    Ok(())
+}
+
+fn cpi_mint(ctx: &Context<ManageAsset>, asset_type:CatalogItem, amount: u64) -> Result<()> {
+    let cpi_accounts = MintTo {
         mint: ctx.accounts.mint_gold.to_account_info(),
         to: ctx.accounts.token_account_gold.to_account_info(),
         authority: ctx.accounts.mint_gold.to_account_info(),
@@ -56,11 +75,12 @@ pub fn trade(ctx: Context<ManageAsset>, trade_index: u8) -> Result<()> {
 
     let seeds  = &[
         b"mint".as_ref(),
-        b"gold".as_ref(),
+        asset_type.as_str().as_bytes(),
         &[ctx.accounts.config.bump_mint_gold]
     ];
     let seeds = &[&seeds[..]];
     let cpi_program = ctx.accounts.token_program.to_account_info();
+
     let cpi_ctx = CpiContext::new_with_signer(
         cpi_program, 
         cpi_accounts, 
@@ -69,7 +89,36 @@ pub fn trade(ctx: Context<ManageAsset>, trade_index: u8) -> Result<()> {
 
     mint_to(
         cpi_ctx, 
-        ctx.accounts.shop_catalog.trading_pairs[trade_index as usize].amount_to_item
+        amount
+    )?;
+    
+    Ok(())
+}
+
+fn cpi_burn(ctx: &Context<ManageAsset>, asset_type:CatalogItem, amount: u64) -> Result<()> {
+    let cpi_accounts = Burn {
+        mint: ctx.accounts.mint_gold.to_account_info(),
+        from: ctx.accounts.token_account_gold.to_account_info(),
+        authority: ctx.accounts.mint_gold.to_account_info(),
+    };
+
+    let seeds  = &[
+        b"mint".as_ref(),
+        asset_type.as_str().as_bytes(),
+        &[ctx.accounts.config.bump_mint_gold]
+    ];
+    let seeds = &[&seeds[..]];
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+
+    let cpi_ctx = CpiContext::new_with_signer(
+        cpi_program, 
+        cpi_accounts, 
+        seeds
+    );
+
+    burn(
+        cpi_ctx, 
+        amount
     )?;
     
     Ok(())
