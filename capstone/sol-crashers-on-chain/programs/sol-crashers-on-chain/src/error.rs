a941793e0c25411a1331111238b42e9d93db6d56
kilogold <bonilla_kelvin@hotmail.com>
2024-06-02 14:23:20 -0400

    Added trading gold for gems

--- capstone/sol-crashers-on-chain/programs/sol-crashers-on-chain/src/error.rs
+++ capstone/sol-crashers-on-chain/programs/sol-crashers-on-chain/src/error.rs
@@ -4,4 +4,7 @@ use anchor_lang::prelude::*;
 pub enum ErrorCode {
     #[msg("Custom error message")]
     CustomError,
+
+    #[msg("An invalid catalog item was parsed.")]
+    InvalidCatalogItemError,
 }
