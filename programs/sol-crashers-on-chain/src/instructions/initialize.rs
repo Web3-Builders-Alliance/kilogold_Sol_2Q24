25a4d6b0f7cef3b0131cb93717e3953c78f9c9cd
kilogold <bonilla_kelvin@hotmail.com>
2024-06-02 12:58:46 -0400

    Adding Gems Mint

--- programs/sol-crashers-on-chain/src/instructions/initialize.rs
+++ programs/sol-crashers-on-chain/src/instructions/initialize.rs
@@ -43,13 +43,31 @@ pub struct Initialize<'info> {
         payer = payer,
         mint::token_program = token_program,
         mint::decimals = 0,
-        mint::authority = mint,
-        mint::freeze_authority = mint,
-        extensions::metadata_pointer::authority = mint,
-        extensions::metadata_pointer::metadata_address = mint,
+        mint::authority = mint_gold,
+        mint::freeze_authority = mint_gold,
+        extensions::metadata_pointer::authority = mint_gold,
+        extensions::metadata_pointer::metadata_address = mint_gold,
         //extensions::close_authority::authority = payer, <- This breaks tests in weird ways.
     )]
-    pub mint: InterfaceAccount<'info, Mint>,
+    pub mint_gold: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        seeds = [
+            b"mint", 
+            b"gems"
+        ],
+        bump,
+        payer = payer,
+        mint::token_program = token_program,
+        mint::decimals = 0,
+        mint::authority = mint_gems,
+        mint::freeze_authority = mint_gems,
+        extensions::metadata_pointer::authority = mint_gems,
+        extensions::metadata_pointer::metadata_address = mint_gems,
+        //extensions::close_authority::authority = payer, <- This breaks tests in weird ways.
+    )]
+    pub mint_gems: InterfaceAccount<'info, Mint>,
 
     #[account(
         init,
@@ -70,17 +88,40 @@ pub fn config(ctx: &mut Context<Initialize>) -> Result<()> {
     let config = &mut ctx.accounts.config;
     config.dev_key = Some(*ctx.accounts.payer.key);
     config.bump_self = ctx.bumps.config;
-    config.bump_mint_gold = ctx.bumps.mint;
+    config.bump_mint_gold = ctx.bumps.mint_gold;
+    config.bump_mint_gems = ctx.bumps.mint_gems;
     config.bump_shop_catalog = ctx.bumps.shop_catalog;
     Ok(())
 }
 pub fn accounts(ctx: &mut Context<Initialize>) -> Result<()> {  
+    set_mint_account(ctx, 
+        "Gold".into(), 
+        "GLD".into(), 
+        "https://gold.com".into(), 
+        &mut ctx.accounts.mint_gold.clone(),
+        b"gold",
+    ctx.bumps.mint_gold
+    )?;
+
+    set_mint_account(ctx,
+        "Gems".into(), 
+        "GEM".into(), 
+        "https://gems.com".into(),
+        &mut ctx.accounts.mint_gems.clone(),
+        b"gems",
+    ctx.bumps.mint_gems
+    )?;
+
+    Ok(())
+}
+
+fn set_mint_account<'a>(ctx: &mut Context<Initialize<'a>>, name: String, symbol: String, uri: String, mint_account: &mut InterfaceAccount<'a, Mint>, mint_seed: &[u8], mint_bump: u8) -> Result<()> {
     // HACK: Mint needs some SOL to pay for the CPI, because Mint is its own authority.
     // TODO: Calcualte how much for CPI.
     // Optimization: Have the payer account to pay for the metadata CPI without changing Mint authority.
     transfer_lamports(
         ctx.accounts.payer.to_account_info(),
-        ctx.accounts.mint.to_account_info(),
+        ctx.accounts.mint_gold.to_account_info(),
         Rent::get()?.minimum_balance(10000000),
         ctx.accounts.system_program.to_account_info(),
     )?;
@@ -93,37 +134,31 @@ pub fn accounts(ctx: &mut Context<Initialize>) -> Result<()> {
     // 3. update the mint account's lamports to the minimum balance using the helper function
     let cpi_accounts = TokenMetadataInitialize {
         token_program_id: ctx.accounts.token_program.to_account_info(),
-        mint: ctx.accounts.mint.to_account_info(),
-        metadata: ctx.accounts.mint.to_account_info(), // metadata account is the mint, since data is stored in mint
-        mint_authority: ctx.accounts.mint.to_account_info(),
-        update_authority: ctx.accounts.mint.to_account_info(),
+        mint: mint_account.to_account_info(),
+        metadata: mint_account.to_account_info(), // metadata account is the mint, since data is stored in mint
+        mint_authority: mint_account.to_account_info(),
+        update_authority: mint_account.to_account_info(),
     };
-
     let seeds  = &[
         b"mint".as_ref(),
-        b"gold".as_ref(),
-        &[ctx.bumps.mint]
+        mint_seed.as_ref(),
+        &[mint_bump]
     ];
     let seeds = &[&seeds[..]];
-
     let cpi_ctx = CpiContext::new_with_signer(
         ctx.accounts.token_program.to_account_info(), 
         cpi_accounts,
         seeds
     );
-        
-    // Step 1
-    token_metadata_initialize(cpi_ctx, "Gold".into(), "GLD".into(), "https://crashers/gld".into())?;
+
+    token_metadata_initialize(cpi_ctx, name, symbol, uri)?;
     msg!("Initialized token metadata for mint account");
 
-
-    // Step 2
-    ctx.accounts.mint.reload()?;
+    mint_account.reload()?;
     msg!("Reloaded mint account after metadata initialization");
 
-    // Step 3
     update_account_lamports_to_minimum_balance(
-        ctx.accounts.mint.to_account_info(),
+        mint_account.to_account_info(),
         ctx.accounts.payer.to_account_info(),
         ctx.accounts.system_program.to_account_info(),
     )?;
